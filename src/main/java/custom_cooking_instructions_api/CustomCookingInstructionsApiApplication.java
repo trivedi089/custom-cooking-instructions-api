24531188120292d284d196401b3c4a4834455770
trivedi089 <satyachbs007@gmail.com>
2025-09-20 17:16:08 +0530

    initial-configuration

---
+++ src/main/java/custom_cooking_instructions_api/CustomCookingInstructionsApiApplication.java
@@ -0,0 +1,13 @@
+package custom_cooking_instructions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomCookingInstructionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomCookingInstructionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/custom_cooking_instructions_api/CustomCookingInstructionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package custom_cooking_instructions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomCookingInstructionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
